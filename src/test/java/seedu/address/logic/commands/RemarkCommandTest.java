6dedb30071911818c185201bb7fc9abf10277092
SamuelFoo <fooenzesamuel@gmail.com>
2024-10-03 14:45:35 +0800

    Add stubs for remark command

---
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -0,0 +1,10 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+
+public class RemarkCommandTest {
+    @Test
+    public void execute() {
+        // assertCommandFailure(new RemarkCommand(), model, MESSAGE_NOT_IMPLEMENTED_YET);
+    }
+}
